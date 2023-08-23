1cea3ebed1c0a2b79d1e78c5967adc867ac42b9b
surabhi <surabhi.das@outlook.com>
2023-08-23 11:24:20 -0400

    Initial version of nature_notes generated by generator-jhipster@7.9.3

---
+++ nature_notes/src/main/java/com/surabhi/naturenotes/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.surabhi.naturenotes.config;
---
+++ nature_notes/src/main/java/com/surabhi/naturenotes/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.surabhi.naturenotes.service.dto;
